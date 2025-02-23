9cee2336d2bb4d950bf892c12f1ff1ae306cf025
Eshika Arya <eshika.arya@gmail.com>
2025-02-23 20:42:48 +0530

    Values and Variables + Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,42 @@
-let js = 'amazing';
-if(js==='amazing') alert('Javascript is FUN!');
+/*
 
-console.log(13+12-11+142);
+let js = 'amazing';
+
+console.log(13 + 12 - 11 + 142);
+console.log(13);
+console.log("XYZ");
+
+let firstName = "Cari";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+*/
+
+
+
+
+// the value holds the datatype in JS,
+// and not the variable ---> dynamic typing
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun); //Dynamic typing
+// console.log(typeof 23);
+// console.log(typeof 'Jane Doe');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun); //Dynamic typing
+
+let year;
+console.log(year);  //undefined
+console.log(typeof year); //undefined
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //object 
+// this is a bug (typeof null) === never corrected due to legacy reasons.
