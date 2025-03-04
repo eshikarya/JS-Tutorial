3d0d3e4f66f17fc179be410050c4c6dc752cff92
Eshika Arya <eshika.arya@gmail.com>
2025-03-04 18:52:31 +0530

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,7 +1,7 @@
-'use strict'; 
-// STRICT MODE ACTIVATION 
-// Should be first line of code
+'use strict';  // STRICT MODE ACTIVATION ---- Should be first line of code
 
+
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,3 +11,36 @@ if (hasDriversLicense) console.log('I can drive');
 // const interface = 'Audio';
 // const private = 123;
 // const if = 123;
+
+*/
+
+
+
+
+// FUNCTIONS
+
+
+
+function logger(){
+    console.log(`I'm Jane Doe.`);
+}
+
+// calling/invoking/running the function
+logger(25); //since function definition does not include parameters 25 passed as argument won't get used! instead of how in java it throws error!!!
+logger('hello',23,132,'what'); //since function definition does not include parameters 25 passed as argument won't get used! instead of how in java it throws error!!!
+logger();
+
+
+function fruitProcessor(apples,oranges){
+    console.log(apples,oranges);
+    // console.log(apples+oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const mixedJuice = fruitProcessor(2,5);
+
+console.log(mixedJuice);
+
+console.log(fruitProcessor(1,6));
+
