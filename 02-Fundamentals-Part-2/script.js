aaaff5b7b60af7caa4d1a6ab02e25d772e7738ae
Eshika Arya <eshika.arya@gmail.com>
2025-03-04 18:17:05 +0530

    Strict Mode Activation

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,13 @@
+'use strict'; 
+// STRICT MODE ACTIVATION 
+// Should be first line of code
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 123;
+// const if = 123;
